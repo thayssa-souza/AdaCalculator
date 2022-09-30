a32220e26c8561b84842a35117030cae4ce14ef5
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-09-30 18:23:24 -0300

    Changin theory for fact on tests with mocks

--- AdaCalculator.Tests/UnitTest1.cs
+++ AdaCalculator.Tests/UnitTest1.cs
@@ -19,44 +19,41 @@ namespace AdaCalculator.Tests
             _sutCalculator = new CalculatorMachine(_calculatorMock.Object);
         }
 
-        [Theory]
-        [InlineData("sum", 44, 46, 100)]
-        [InlineData("sum", 12.4, 2.1, 14.5)]
-        [InlineData("sum", -2, 3, 1)]
-        public void CalculatorMachine_SummingTwoNumbersWithMocks_ShouldBeResult(string sum, double numb1, double numb2, double result)
+        [Fact]
+        public void CalculatorMachine_SummingTwoNumbersWithMocks_ShouldBeResultCorrect()
         {
-           _calculatorMock.Setup(x => x.Calculate(sum, numb1, numb2)).Returns((sum, result));
-           var sutResult = _sutCalculator.Calculate(sum, numb1, numb2);
-           _calculatorMock.Verify(x => x.Calculate(sum, numb1, numb2), Times.Once);
+            _calculatorMock.Setup(x => x.Calculate("sum", 27, -2)).Returns(("sum", 25));
+            var sutResult = _sutCalculator.Calculate("sum", 27, -2);
+            _calculatorMock.Verify(x => x.Calculate("sum", 27, -2), Times.Once);
+
+            Assert.Equal(("sum", 25), sutResult);
         }
 
         [Theory]
         [InlineData("subtract", 21, 3, 18)]
         [InlineData("subtract", 24, -24, 48)]
         [InlineData("subtract", 2, 0, 2)]
-        public void CalculatorMachine_SubtractingTwoNumbers_ShouldBeResult(string subtract, double numb1, double numb2, double result)
+        public void CalculatorMachine_SubtractingTwoNumbers_ShouldBeResultCorrect(string subtract, double numb1, double numb2, double result)
         {
             var sut = new CalculatorMachine();
             var resultSut = sut.Calculate(subtract, numb1, numb2);
             resultSut.ShouldBe((subtract, result));
         }
 
-        [Theory]
-        [InlineData("divide", 15, 3, 5)]
-        [InlineData("divide", 5, 2, 2.5)]
-        [InlineData("divide", 12, 1, 12)]
-        public void CalculatorMachine_DivingTwoNumbersWithMocks_ShouldBeResult(string divide, double numb1, double numb2, double result)
+        [Fact]
+        public void CalculatorMachine_DivingTwoNumbersWithMocks_ShouldBeResultCorrect()
         {
-            _calculatorMock.Setup(x => x.Calculate(divide, numb1, numb2)).Returns((divide, result));
-            var sutResult = _sutCalculator.Calculate(divide, numb1, numb2);
-            _calculatorMock.Verify(x => x.Calculate(divide, numb1, numb2), Times.Once);
+            _calculatorMock.Setup(x => x.Calculate("divide", 8, 2)).Returns(("divide", 4));
+            var sutResult = _sutCalculator.Calculate("divide", 8, 2);
+            _calculatorMock.Verify(x => x.Calculate("divide", 8, 2), Times.Once);
+            Assert.Equal(("divide", 4), sutResult);
         }
 
         [Theory]
         [InlineData("multiply", 12, 0, 0)]
         [InlineData("multiply", 3.5, 2, 7)]
         [InlineData("multiply", 2, 8, 16)]
-        public void CalculatorMachine_MultiplyingTwoNumbers_ShouldBeResult(string multiply, double numb1, double numb2, double result)
+        public void CalculatorMachine_MultiplyingTwoNumbers_ShouldBeResultCorrect(string multiply, double numb1, double numb2, double result)
         {
             var sut = new CalculatorMachine();
             var resultSut = sut.Calculate(multiply, numb1, numb2);
